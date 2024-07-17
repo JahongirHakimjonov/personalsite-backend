48d80b8ad9b13238e1c7230f0556497aaed4be66
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-07-17 17:57:03 +0500

    fix

--- apps/portfolio/views/contact.py
+++ apps/portfolio/views/contact.py
@@ -1,4 +1,3 @@
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,10 +9,6 @@ from apps.portfolio.serializers.contact import ContactInfoSerializer
 class ContactInfoView(APIView):
     permission_classes = [AllowAny]
 
-    @swagger_auto_schema(
-        operation_summary="Get contact information",
-        responses={200: ContactInfoSerializer()},
-    )
     def get(self, request):
         contact_info = ContactInfo.objects.first()
         serializer = ContactInfoSerializer(contact_info)
