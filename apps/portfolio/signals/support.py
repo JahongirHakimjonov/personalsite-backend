afe8618483e1c910fa19e098fdf1c0dfd19be871
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-07-18 11:01:02 +0500

    fix: smtp add

--- apps/portfolio/signals/support.py
+++ apps/portfolio/signals/support.py
@@ -8,15 +8,13 @@ from apps.portfolio.models import Support
 
 @receiver(pre_save, sender=Support)
 def send_email_on_answer(sender, instance, **kwargs):
-    if instance.pk and instance.answer:
-        original_instance = sender.objects.get(pk=instance.pk)
-        if not original_instance.answer == instance.answer:
-            send_mail(
-                subject="Murojaat uchun rahmat !!!",
-                message=instance.answer,
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[instance.email],
-            )
-            print(f"Email sent to {instance.email}")
-            instance.is_read = True
-            instance.save()
+    if instance.answer:
+        send_mail(
+            subject="Murojaat uchun rahmat !!!",
+            message=instance.answer,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[instance.email],
+        )
+        print(f"Email sent to {instance.email}")
+        instance.is_read = True
+        instance.save()
