e8a764a6448e2436fd13b74cd9502b1e67221095
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-07-18 11:03:48 +0500

    fix: smtp add

--- apps/portfolio/signals/support.py
+++ apps/portfolio/signals/support.py
@@ -8,7 +8,7 @@ from apps.portfolio.models import Support
 
 @receiver(pre_save, sender=Support)
 def send_email_on_answer(sender, instance, **kwargs):
-    if instance.answer:
+    if instance.answer and not instance.is_read:
         send_mail(
             subject="Murojaat uchun rahmat !!!",
             message=instance.answer,
